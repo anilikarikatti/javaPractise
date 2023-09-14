0474c45d5c7883010984200f06f42d33377eaf9e
anilikarikatti <anilkarikatti333@gmail.com>
2023-09-14 18:24:49 +0530

    sorting added

---
+++ Swing.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+class Swing{
+    public static void main(String args[]){
+        JFrame   myWindow ;
+        myWindow = new JFrame();      
+        myWindow.setSize(300,200);
+
+        myWindow.setTitle("anil java learning");
+
+        // myWindow.setValue("Asdk");
+
+        myWindow.setVisible(true);
+
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+// import javax.swing.*;
+// class
+// Ch2Sample1 {
+// public static void main(String[] args) {
+// JFrame
+// myWindow;
+// myWindow = new JFrame();
+// myWindow.setSize(300, 200);
+// myWindow.setTitle("My First Java Program");
+// myWindow.setVisible(true);
+// }
+// }
---
+++ TestingMain.java
@@ -0,0 +1,25 @@
+
+
+class TestingMain{
+
+
+      static void display(){
+        int a = 100;
+        double f =3.142f;
+
+        // byte b = a;
+        // f = 10111.123;
+        System.out.println("float value  = " +a);
+    }
+
+    public static void main (String args[]){
+
+        System.out.println("Hello");
+
+         display();
+
+    }
+
+   
+
+}

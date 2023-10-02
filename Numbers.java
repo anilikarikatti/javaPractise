108169e58f6ae11120f89cae21bd28c21d0d3f3d
anilikarikatti <anilkarikatti333@gmail.com>
2023-10-02 17:41:37 +0530

    numbers

---
+++ Numbers.java
@@ -0,0 +1,20 @@
+class Numbers {
+
+    public static void main(String args[]){
+
+        for(int i=1;i<=9;i++){
+            System.out.print(" ".repeat(9-i));
+
+            for(int j=1 ; j<=i ; j++){
+            
+            System.out.print("*");
+            System.out.print(" ");
+
+
+            }
+
+            System.out.print("\n");
+
+        }
+    }
+}
